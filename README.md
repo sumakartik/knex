9d41425dbcd0657d883e3d88b224cc339ef14ac0
Suma Kartik <pair+kartiksuma@forcenex.us>
2022-03-08 11:29:23 -0500

    initialized migrations

---
+++ README.md
@@ -0,0 +1,4 @@
+## Knex.js
+Your purpose is to give a web server (an Express application) read and write access to a database. So far, you have had to create the database and its schema separately from the application but, by using a tool like Knex.js, a JavaScript-based database query and schema builder, you can abstract away the syntax for those operations into JavaScript.
+
+This sounds interesting, so let us create an npm project and think about what you need in order to get your database querying web server up and running. You'll be submitting a link to this GitHub repository at the end of this lesson, so be sure to follow along closely and push all of your changes.
---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      connection: "postgres://postgres:docker@localhost:5432/postgres",
+      // filename: './dev.sqlite3'
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
